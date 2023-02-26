e6bdc98edeaaba0b36e642c604c6f69aa7f72e7b
Венера <125314304+VeneraChepok@users.noreply.github.com>
2023-02-26 21:12:27 +0300

    new commit

--- Test.java
+++ Test.java
@@ -2,5 +2,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("HW");
         System.out.println("rr");
+        System.out.println("rrrr");
+
     }
 }
